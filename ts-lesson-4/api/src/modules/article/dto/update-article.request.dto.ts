bf41a5729aa2a17709edd9e6be009a9f03224bd0
maccchan4869 <ymatsio554@gmail.com>
2021-07-23 08:51:40 +0900

    不具合修正

--- ts-lesson-4/api/src/modules/article/dto/update-article.request.dto.ts
+++ ts-lesson-4/api/src/modules/article/dto/update-article.request.dto.ts
@@ -1,9 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 export class updateArticleRequestDto {
   @ApiProperty()
-  userId!: string;
-
-  @ApiProperty()
   title!: string;
 
   @ApiProperty()
