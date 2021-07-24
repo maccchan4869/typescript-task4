bba4a5289939dc1c5fc9449369877569eae7eb86
maccchan4869 <ymatsio554@gmail.com>
2021-07-24 11:31:41 +0900

    修正漏れ

--- ts-lesson-4/api/src/modules/tag/interface/tag.service.interface.ts
+++ ts-lesson-4/api/src/modules/tag/interface/tag.service.interface.ts
@@ -1,17 +1,16 @@
 import { DeleteResult } from 'typeorm';
-import { createTagRequestDto } from '../dto/tag.request.dto';
+import { TagRequestDto } from '../dto/tag.request.dto';
 import { TagResponseDto } from '../dto/tag.response.dto';
 import { TagsResponseDto } from '../dto/tags.response.dto';
-import { updateTagRequestDto } from '../dto/update-tag.request.dto';
 
 export interface ITagService {
-  createTag(param: createTagRequestDto): Promise<TagResponseDto>;
+  createTag(param: TagRequestDto): Promise<TagResponseDto>;
   
   getTags(): Promise<TagsResponseDto>;
 
   findTag(tagId: number): Promise<TagResponseDto>;
 
-  updateTag(tagId: number, param: updateTagRequestDto): Promise<TagResponseDto>;
+  updateTag(tagId: number, param: TagRequestDto): Promise<TagResponseDto>;
 
   deleteTag(tagId: number): Promise<DeleteResult>;
 }
--- ts-lesson-4/api/src/modules/user/interface/user.service.interface.ts
+++ ts-lesson-4/api/src/modules/user/interface/user.service.interface.ts
@@ -1,14 +1,13 @@
-import { createUserRequestDto } from '../dto/create-user.request.dto';
-import { updateUserRequestDto } from '../dto/user.request.dto';
+import { UserRequestDto } from '../dto/user.request.dto';
 import { UserResponseDto } from '../dto/user.response.dto';
 import { UsersResponseDto } from '../dto/users.response.dto';
 
 export interface IUserService {
-  createUser(param: createUserRequestDto): Promise<UserResponseDto>;
+  createUser(param: UserRequestDto): Promise<UserResponseDto>;
 
   getUsers(): Promise<UsersResponseDto>;
 
   findUser(userId: string): Promise<UserResponseDto>;
 
-  updateUser(userId: string, param: updateUserRequestDto): Promise<UserResponseDto>;
+  updateUser(userId: string, param: UserRequestDto): Promise<UserResponseDto>;
 }
